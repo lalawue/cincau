e9160668d96fc926c9bbdc1d89819812a87b75a1
lalawue <suchaaa@gmail.com>
2020-08-27 01:13:07 +0800

    add tokyocabinet DBM

--- build_vendor.sh
+++ build_vendor.sh
@@ -41,7 +41,10 @@ HP_DIR=vendor/hyperparser
 OPENSSL_DIR=vendor/openssl
 CJSON_DIR=vendor/cjson
 CJSON_FILES="lua_cjson.c strbuf.c fpconv.c"
+CABINET_DIR=vendor/cabinet
 VD_DIR=cincau/vendor
+
+LUA_FLAGS="-I$LUAJIT_INC_DIR -L$LUAJIT_LIB_DIR -l$LUAJIT_LIB_NAME"
 
 mkdir -p $VD_DIR
 if [ ! -d "$MNET_DIR" ]; then
@@ -56,6 +59,9 @@ fi
 if [ ! -d "$OPENSSL_DIR" ]; then
     git clone --depth 1 --recurse https://github.com/zhaozg/lua-openssl.git $OPENSSL_DIR
 fi
+if [ ! -d "$CABINET_DIR" ]; then
+   git clone --depth 1 https://github.com/lalawue/lua-tokyocabinet.git $CABINET_DIR
+fi
 
 # make
 echo_run "make lib -C $MNET_DIR"
@@ -64,6 +70,8 @@ echo_run "make -C $MDNSCNT_DIR"
 echo_run "make -C $HP_DIR"
 echo_run "cd $CJSON_DIR && gcc -o libcjson.$SUFFIX -O3 -shared -fPIC -I$LUAJIT_INC_DIR -L$LUAJIT_LIB_DIR -l$LUAJIT_LIB_NAME $CJSON_FILES && cd -"
 echo_run "make -C $OPENSSL_DIR"
+echo_run "cd $CABINET_DIR && ./build.sh tokyocabinet && cd -"
+echo_run "cd $CABINET_DIR && ./build.sh lua $LUA_FLAGS && cd -" 
 # copy
 echo_run "cp -f $MNET_DIR/build/libmnet.* $VD_DIR/libmnet.$SUFFIX"
 echo_run "cp -f $MNET_DIR/extension/luajit/ffi_mnet.lua $VD_DIR"
@@ -73,4 +81,6 @@ echo_run "cp -f $HP_DIR/hyperparser.* $VD_DIR/libhyperparser.$SUFFIX"
 echo_run "cp -f $HP_DIR/ffi_hyperparser.lua $VD_DIR"
 echo_run "cp -f $CJSON_DIR/libcjson.$SUFFIX $VD_DIR/libcjson.$SUFFIX"
 echo_run "cp -f $OPENSSL_DIR/openssl.so $VD_DIR/libopenssl.$SUFFIX"
+echo_run "cp -f $CABINET_DIR/tokyocabinet-1.4.48/libtokyocabinet.9.11.0.* $VD_DIR/libtokyocabinet.$SUFFIX"
+echo_run "cp -f $CABINET_DIR/cabinet.so $VD_DIR/libcabinet.$SUFFIX"
 echo "build and copy done"
