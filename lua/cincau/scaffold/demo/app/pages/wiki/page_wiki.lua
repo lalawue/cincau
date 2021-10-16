173bb22caf43aa9cfdb83f4cc5adb238d80842a9
lalawue <suchaaa@gmail.com>
2021-10-17 00:26:00 +0800

    fix wiki page

--- lua/cincau/scaffold/demo/app/pages/wiki/page_wiki.lua
+++ lua/cincau/scaffold/demo/app/pages/wiki/page_wiki.lua
@@ -18,7 +18,7 @@ function Page:process(config, req, response, params)
         -- set header before appendBody
         response:setHeader("Content-Type", "text/html")
         -- render page content
-        local page_content = Render:render(self.htmlSpec, {
+        local page_content = Render:render(self:templteHTML(), {
         })
         -- append body as chunked data
         response:appendBody(page_content)
@@ -26,52 +26,51 @@ function Page:process(config, req, response, params)
 end
 
 -- using default tags
-function Page:htmlSpec()
-    return {
-        html {
-            doctype "html",
-            head {
-                meta {'[charset=utf-8]'},
-                meta {'[name=viewport][content=width=device-width, initial-scale=1]'},
-                link {'[rel=stylesheet][href=/css/doc.css]'},
-                style {[[.editor-toolbar {
-                    background: #fff;
-                }
-                button {
-                    padding: 10px 15px 12px 15px;
-                    border-radius: 8px;
-                }
-                h1,h2,h3,h4,h5,h6,p,ul,pre {
-                    margin-left: 0;
-                    margin-right: 0;
-                }]]},
-            },
-            body {
-                div {{'[class=fixed top-0]'},
-                div {{'[id=btn_left][class=fixed left-0 mt2 ml2]'},
-                    button {{"[onclick=window.location.href='/';]"}, "Home"},
-                    br,
-                    button {{"[onclick=window.location.href='/wiki';][class=mt1]"}, "Index"},
-                },
-                    div {{'[id=btn_right][class=fixed right-0 mt2 mr2]'}},
-                },
-                div {{'[class=mx-auto]'},
-                    h1 {{'[id=wiki_title][class=center]'},
-                    }
-                },
-                hr,
-                br,
-                div {{'[id=main_container][class=lg-col-8 md-col-10 mx-auto]'},
-                },
-                link { '[rel=stylesheet][href=https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css]'},
-                link {'[rel=stylesheet][href=https://unpkg.com/basscss@8.0.2/css/basscss.min.css]'},
-                script {{'[src=https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js]'}},
-                script {{'[src=https://unpkg.com/htm@3.1.0/dist/htm.js]'}},
-                script {{'[src=https://unpkg.com/mithril@2.0.4/mithril.min.js]'}},
-                script {{'[src=/js/wiki_cnt.js]'}},
-            }
+function Page:templteHTML()
+    return
+[[<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width initial-scale=1" />
+        <link rel="stylesheet" type="text/css" href="/css/doc.css" />
+        <style>
+        .editor-toolbar {
+            background: #fff;
         }
-    }
+        button {
+            padding: 10px 15px 12px 15px;
+            border-radius: 8px;
+        }
+        h1,h2,h3,h4,h5,h6,p,ul,pre {
+            margin-left: 0;
+            margin-right: 0;
+        }
+        </style>
+    </head>
+    <body>
+        <div class="fixed top-0">
+            <div id="btn_left" class="fixed left-0 mt2 ml2">
+                <button onclick="window.location.href='/';">Home</button><br/>
+                <button onclick="window.location.href='/wiki';" class="mt1">Index</button>
+            </div>
+            <div id="btn_right" class="fixed right-0 mt2 mr2">
+            </div>
+        </div>
+        <div class="mx-auto">
+            <h1 id="wiki_title" class="center"></h1>
+        </div>
+        <hr/>
+        <br/>
+        <div id="main_container" class="lg-col-8 md-col-10 mx-auto">
+        </div>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://unpkg.com/basscss@8.0.2/css/basscss.min.css" />
+        <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+        <script src="https://unpkg.com/htm@3.1.0/dist/htm.js"></script>
+        <script src="https://unpkg.com/mithril@2.0.4/mithril.min.js"></script>
+        <script src="/js/wiki_cnt.js"></script>
+    </body>
+</html>]]
 end
 
 return Page
